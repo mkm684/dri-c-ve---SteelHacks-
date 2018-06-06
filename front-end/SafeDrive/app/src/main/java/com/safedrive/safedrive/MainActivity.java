e1bbaaefac3dd2da1106cb61384d568b4949ae8b
mkm684 <mkm684@mun.ca>
2018-06-06 00:58:27 -0230

    front end

---
+++ front-end/SafeDrive/app/src/main/java/com/safedrive/safedrive/MainActivity.java
@@ -0,0 +1,98 @@
+package com.safedrive.safedrive;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Surface;
+import android.view.SurfaceView;
+import android.widget.Toast;
+import com.amazonaws.mobile.client.AWSMobileClient;
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.JavaCameraView;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
+
+public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2{
+
+    CameraBridgeViewBase cameraBridgeViewBase;
+    Mat mat1, mat2, mat3;
+    BaseLoaderCallback baseLoaderCallback;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        AWSMobileClient.getInstance().initialize(this).execute();
+        cameraBridgeViewBase = (JavaCameraView) findViewById(R.id.mycamerview);
+        cameraBridgeViewBase.setVisibility(SurfaceView.VISIBLE);
+        cameraBridgeViewBase.setCvCameraViewListener(this);
+
+        baseLoaderCallback = new BaseLoaderCallback(this) {
+            @Override
+            public void onManagerConnected(int status) {
+                switch (status){
+                    case BaseLoaderCallback.SUCCESS:
+                        mat1 = new Mat();
+                        mat2 = new Mat();
+                        mat3 = new Mat();
+                        cameraBridgeViewBase.enableView();
+                    default:
+                        super.onManagerConnected(status);
+                        break;
+                }
+            }
+        };
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        mat1 = inputFrame.rgba();
+        //rotate the fram to 90 degrees
+        Core.transpose(mat1,mat2);
+        Core.flip(mat2, mat1,1);
+
+        return mat1;
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+        mat1.release();
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(cameraBridgeViewBase != null){
+            cameraBridgeViewBase.disableView();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (!OpenCVLoader.initDebug()) {
+            Log.d("OpenCV", "Internal OpenCV library not found. Using OpenCV Manager for initialization");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, this, baseLoaderCallback);
+        } else {
+            Log.d("OpenCV", "OpenCV library found inside package. Using it!");
+            baseLoaderCallback.onManagerConnected(BaseLoaderCallback.SUCCESS);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(cameraBridgeViewBase != null){
+            cameraBridgeViewBase.disableView();
+        }
+    }
+}
